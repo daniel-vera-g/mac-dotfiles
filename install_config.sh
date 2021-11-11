97abcff88911e75518d2dac5a4e9be8fe4234280
Daniel VG <danielvg@Daniels-MacBook-Air.local>
2021-11-11 18:18:40 +0100

    Install: All things added

--- install_config.sh
+++ install_config.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
-ln -sfv $PWD/configs/karabiner ~/.config/
-
+# Shell configs
 ln -sfv $PWD/configs/.zshrc ~
+ln -sfv $PWD/configs/.alias ~
 ln -sfv $PWD/configs/.profile ~
+
+# Apps
+ln -sfv $PWD/configs/.tmux.conf ~
+ln -sfv $PWD/configs/karabiner ~/.config/
+ln -sfv $PWD/configs/nvim ~/.config/
+ln -sfv $PWD/configs/kitty ~/.config/
