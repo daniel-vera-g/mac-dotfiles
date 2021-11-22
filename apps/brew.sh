7927ec8649c16f32b56075b5f0103ed150e416e6
daniel-vera-g <danielveragi@gmail.com>
2021-11-22 23:30:58 +0100

    Remove zsh syntax highlight

--- apps/brew.sh
+++ apps/brew.sh
@@ -4,7 +4,7 @@
 	brew install zsh gh node htop bat exa fd fzf lazygit nnn ripgrep tmux rclone go-task/tap/go-task docker-compose go poppler ncspot macfuse spaceship bat exa pandoc zsh-syntax-highlighting && \
 	# Postinstall for fzf
 	/usr/local/opt/fzf/install && \
-	brew install --cask docker obsidian spotify brave-browser iterm2 rectangle karabiner-elements visual-studio-code jumpcut discord kitty anki blender mpv microsoft-teams zoom zsh-syntax-highlighting obs && \
+	brew install --cask docker obsidian spotify brave-browser iterm2 rectangle karabiner-elements visual-studio-code jumpcut discord kitty anki blender mpv microsoft-teams zoom obs && \
 	brew install neovim --HEAD && \
 	brew tap homebrew/cask-fonts && brew install --cask font-hack-nerd-font && \ # Nerdfonts
   brew install --cask --appdir=/Applications megacmd 
