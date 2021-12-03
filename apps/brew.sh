a75a3a839cb12c89cf92056afb47614c8ef7097c
danielVg <danielveragi@gmail.com>
2021-12-03 15:45:16 +0100

    Mega and some other apps

--- apps/brew.sh
+++ apps/brew.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
-	brew install zsh gh node htop bat exa fd fzf lazygit nnn ripgrep tmux rclone go-task/tap/go-task docker-compose go poppler ncspot macfuse spaceship bat exa pandoc zsh-syntax-highlighting && \
+	brew install zsh gh node htop bat exa fd fzf lazygit nnn ripgrep tmux rclone go-task/tap/go-task docker-compose go poppler ncspot macfuse spaceship bat exa pandoc zsh-syntax-highlighting gawk delta translate-shell && \
 	# Postinstall for fzf
 	/usr/local/opt/fzf/install && \
-	brew install --cask docker obsidian spotify brave-browser iterm2 rectangle karabiner-elements visual-studio-code jumpcut discord kitty anki blender mpv microsoft-teams zoom obs alfred && \
+	brew install --cask docker obsidian spotify brave-browser iterm2 rectangle karabiner-elements visual-studio-code jumpcut discord kitty anki blender vlc microsoft-teams zoom obs alfred megasync background-music && \
+	 brew install --cask --appdir=/Applications megacmd && \
 	brew install neovim --HEAD && \
 	brew tap homebrew/cask-fonts && brew install --cask font-hack-nerd-font && \ # Nerdfonts
-  brew install --cask --appdir=/Applications megacmd 
