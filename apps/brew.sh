cc98ce81f1db5f7a6894993c09a2c6003c946304
Daniel VG <danielveragi@gmail.com>
2022-02-01 10:16:51 +0100

    mac 2 (#8)
    * feat: notifications on mac
    
    * apps: tree for dirs

--- apps/brew.sh
+++ apps/brew.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
-  brew install zsh gh node htop bat exa fd fzf lazygit nnn ripgrep tmux rclone go-task/tap/go-task docker-compose go poppler ncspot macfuse spaceship bat exa pandoc zsh-syntax-highlighting gawk delta translate-shell rga pandoc poppler tesseract ffmpeg thefuck pngpaste gnupg kubectl ocrmypdf tree && \
+  brew install zsh gh node htop bat exa fd fzf lazygit nnn ripgrep tmux rclone go-task/tap/go-task docker-compose go poppler ncspot macfuse spaceship bat exa pandoc zsh-syntax-highlighting gawk git-delta translate-shell rga pandoc poppler tesseract ffmpeg thefuck pngpaste gnupg kubectl ocrmypdf tree && \
   # Postinstall for fzf
   /usr/local/opt/fzf/install && \
     brew install --cask docker obsidian spotify brave-browser iterm2 rectangle karabiner-elements visual-studio-code jumpcut discord kitty anki blender vlc microsoft-teams zoom obs alfred megasync background-music signal cryptomator jetbrains-toolbox basictex && \
