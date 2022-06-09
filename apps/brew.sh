9d62f9010f3ff2c449709eea1a99bb6f539ae33b
danielVg <danielveragi@gmail.com>
2022-06-09 09:15:19 +0200

    fix: use full tex distribution

--- apps/brew.sh
+++ apps/brew.sh
@@ -4,7 +4,7 @@
   brew install zsh gh node htop bat exa fd fzf lazygit nnn ripgrep tmux rclone go-task/tap/go-task docker-compose go poppler ncspot macfuse spaceship bat exa pandoc zsh-syntax-highlighting gawk git-delta translate-shell rga pandoc poppler tesseract ffmpeg thefuck pngpaste gnupg kubectl ocrmypdf tree scrcpy android-platform-tools translate-shell && \
   # Postinstall for fzf
   /usr/local/opt/fzf/install && \
-    brew install --cask docker obsidian spotify brave-browser iterm2 rectangle karabiner-elements visual-studio-code jumpcut discord kitty anki blender vlc microsoft-teams zoom obs alfred megasync background-music signal cryptomator jetbrains-toolbox basictex portfolioperformance utm calibre && \
+    brew install --cask docker obsidian spotify brave-browser iterm2 rectangle karabiner-elements visual-studio-code jumpcut discord kitty anki blender vlc microsoft-teams zoom obs alfred megasync background-music signal cryptomator jetbrains-toolbox mactex portfolioperformance utm calibre && \
     brew install --cask --appdir=/Applications megacmd && \
     brew install neovim --HEAD && \
     brew tap homebrew/cask-fonts && brew install --cask font-hack-nerd-font && \ # Nerdfonts
