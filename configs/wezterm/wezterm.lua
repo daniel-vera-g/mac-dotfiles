ee5c719e4f7d3162af1206de2861bd997363f7f5
danielVg <danielveragi@gmail.com>
2023-12-28 15:46:24 +0100

    audible bell disabled (And some formatting as it seems...)

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,52 +1,54 @@
 local wezterm = require("wezterm")
 return {
-	hide_tab_bar_if_only_one_tab = true,
-	default_cwd = os.getenv("HOME") .. "/Downloads",
+  hide_tab_bar_if_only_one_tab = true,
+  default_cwd = os.getenv("HOME") .. "/Downloads",
 
-	-- Font related
-	font = wezterm.font("JetBrainsMono Nerd Font"),
-	font_size = 21,
+  audible_bell = "Disabled",
 
-	-- For German keyboard layout. Refer to:
-	-- https://github.com/wez/wezterm/issues/410#issuecomment-770507001
-	-- https://wezfurlong.org/wezterm/config/keys.html#macos-left-and-right-option-key
-	-- Use ~ on properly on German keyboard layout.
-	send_composed_key_when_left_alt_is_pressed = true,
-	send_composed_key_when_right_alt_is_pressed = false,
+  -- Font related
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font_size = 21,
 
-	-- Don't wait for second key press to send the composed key.
-	-- TODO test in future whether dead keys works again...
-	-- use_dead_keys = false,
+  -- For German keyboard layout. Refer to:
+  -- https://github.com/wez/wezterm/issues/410#issuecomment-770507001
+  -- https://wezfurlong.org/wezterm/config/keys.html#macos-left-and-right-option-key
+  -- Use ~ on properly on German keyboard layout.
+  send_composed_key_when_left_alt_is_pressed = true,
+  send_composed_key_when_right_alt_is_pressed = false,
 
-	-- Also for Tmux & co
-	mux_output_parser_buffer_size = 1000000,
+  -- Don't wait for second key press to send the composed key.
+  -- TODO test in future whether dead keys works again...
+  -- use_dead_keys = false,
 
-	-- No padding on the sides
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+  -- Also for Tmux & co
+  mux_output_parser_buffer_size = 1000000,
 
-	adjust_window_size_when_changing_font_size = false,
-	keys = {
-		{ key = "+", mods = "CTRL", action = "IncreaseFontSize" },
-		{ key = "-", mods = "CTRL", action = "DecreaseFontSize" },
-		{ key = "Tab", mods = "SHIFT|CTRL", action = wezterm.action({ ActivateTabRelative = 1 }) },
-		{ key = "Tab", mods = "SHIFT|CTRL", action = wezterm.action({ ActivateTabRelative = -1 }) },
-		{ key = "9", mods = "SUPER", action = "ShowTabNavigator" },
+  -- No padding on the sides
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
 
-		-- Disable keys --
+  adjust_window_size_when_changing_font_size = false,
+  keys = {
+    { key = "+",   mods = "CTRL",       action = "IncreaseFontSize" },
+    { key = "-",   mods = "CTRL",       action = "DecreaseFontSize" },
+    { key = "Tab", mods = "SHIFT|CTRL", action = wezterm.action({ ActivateTabRelative = 1 }) },
+    { key = "Tab", mods = "SHIFT|CTRL", action = wezterm.action({ ActivateTabRelative = -1 }) },
+    { key = "9",   mods = "SUPER",      action = "ShowTabNavigator" },
 
-		-- Braces on german keyboard layout
-		{ key = "2", mods = "ALT", action = "DisableDefaultAssignment" },
-		{ key = "3", mods = "ALT", action = "DisableDefaultAssignment" },
-		{ key = "4", mods = "ALT", action = "DisableDefaultAssignment" },
-		{ key = "5", mods = "ALT", action = "DisableDefaultAssignment" },
-		{ key = "6", mods = "ALT", action = "DisableDefaultAssignment" },
-		{ key = "7", mods = "ALT", action = "DisableDefaultAssignment" },
-		{ key = "8", mods = "ALT", action = "DisableDefaultAssignment" },
-		{ key = "9", mods = "ALT", action = "DisableDefaultAssignment" },
-	},
+    -- Disable keys --
+
+    -- Braces on german keyboard layout
+    { key = "2",   mods = "ALT",        action = "DisableDefaultAssignment" },
+    { key = "3",   mods = "ALT",        action = "DisableDefaultAssignment" },
+    { key = "4",   mods = "ALT",        action = "DisableDefaultAssignment" },
+    { key = "5",   mods = "ALT",        action = "DisableDefaultAssignment" },
+    { key = "6",   mods = "ALT",        action = "DisableDefaultAssignment" },
+    { key = "7",   mods = "ALT",        action = "DisableDefaultAssignment" },
+    { key = "8",   mods = "ALT",        action = "DisableDefaultAssignment" },
+    { key = "9",   mods = "ALT",        action = "DisableDefaultAssignment" },
+  },
 }
